b4ffde9d1541fb7ec088c6534d2849c0643c59c2
AlfajGodad <alfajgodad7@gmail.com>
2026-09-14 12:04:06 +0530

    Clean framework structure and configuration

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -3,8 +3,8 @@ package testBase;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Properties;
@@ -19,106 +19,249 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class BaseClass {
-	public static WebDriver driver;
-	public Logger logger;
-	public Properties p;
-	@BeforeClass(groups= {"Sanity", "Master", "DataDriven"})
-	@Parameters({"browser","os"})
-	public void setUp(String br, String os) throws IOException {
-		logger= LogManager.getLogger(this.getClass());
-		FileReader file= new FileReader("./src//test//resources//confi.properties");
-		p= new Properties();
-		p.load(file);
-		String hubUrl="http://192.168.31.192:4444/wd/hub";
-		if(p.getProperty("excecution_Env").equalsIgnoreCase("remote")) {
-			DesiredCapabilities cap= new DesiredCapabilities();
-			if(os.equals("windows") ) {
-			cap.setPlatform(Platform.WIN11);
-			
-			}
-			else if(os.equals("linux")  ) {
-				cap.setPlatform(Platform.LINUX);
-				
-				}
-			else if(os.equals("mac")  ) {
-				cap.setPlatform(Platform.MAC);
-				
-				}
-			
-			switch (br.toLowerCase()){
-			case "chrome": cap.setBrowserName("chrome");
-			 driver= new RemoteWebDriver(new URL(hubUrl), cap); break;
-			case "firefox": cap.setBrowserName("firefox");
-			 driver= new RemoteWebDriver(new URL(hubUrl), cap);break;
-			case "edge": cap.setBrowserName("MicrosoftEdge");
-			 driver= new RemoteWebDriver(new URL(hubUrl), cap);break;
-			 default: System.out.println("Invalid browser"); return;
-			}
-			
-			
-		}
-		
-		
-		if(p.getProperty("excecution_Env").equalsIgnoreCase("local")) {
-		
-		switch(br.toLowerCase()) {
-		case "chrome": WebDriverManager.chromedriver().setup();
-		driver= new ChromeDriver(); break;
-		case "edge":
-			System.setProperty("webdriver.edge.driver", "C:\\Study\\API Testing\\msedgedriver.exe");
-			 driver = new EdgeDriver(); break;
-		default : System.out.println("Invalid browser");	return; 
-		}
-		}
-		
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-		driver.get(p.getProperty("appUrl"));
-		driver.manage().window().maximize();
-	}
-	
-	@AfterClass(groups= {"Sanity", "Master", "DataDriven"})
-	public void tearDown() {
-		driver.quit();
-		
-	}
-	
-	public String randomString() {
-		String random= RandomStringUtils.randomAlphabetic(4);
-		return random;
-	}
-	
-	public String randomAlphaNumeric() {
-		return RandomStringUtils.randomAlphanumeric(5)+ "#";
-	}
-	public String randomNumber() {
-		return RandomStringUtils.randomNumeric(10);
-	}
-	
-	public static String screenCapture(String name) throws IOException {
-		DateTimeFormatter formatter =
-                DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss");
 
-        String timeStamp = LocalDateTime.now().format(formatter);
-		
-		TakesScreenshot sc= (TakesScreenshot) driver;
-		File scr=sc.getScreenshotAs(OutputType.FILE);
-			File file= new File(".\\ScreenShots\\FailedTest " +name + timeStamp +".png");
-			
-			FileUtils.copyFile(scr, file);
-			String path= file.getAbsolutePath();
-			return path;
-			
-		
-	}
+    public static WebDriver driver;
 
+    public Logger logger;
+    public Properties p;
+
+    @BeforeClass(alwaysRun = true)
+    @Parameters({"browser", "os"})
+    public void setUp(
+            @Optional("chrome") String browser,
+            @Optional("windows") String os)
+            throws IOException {
+
+        logger = LogManager.getLogger(this.getClass());
+
+        // Load configuration
+        FileReader file =
+                new FileReader("./src/test/resources/config.properties");
+
+        p = new Properties();
+        p.load(file);
+        file.close();
+
+        String executionEnv =
+                p.getProperty("executionEnv", "local");
+
+        logger.info("Execution environment: " + executionEnv);
+        logger.info("Browser: " + browser);
+        logger.info("Operating System: " + os);
+
+        if (executionEnv.equalsIgnoreCase("remote")) {
+
+            driver = initializeRemoteDriver(browser, os);
+
+        } else if (executionEnv.equalsIgnoreCase("local")) {
+
+            driver = initializeLocalDriver(browser);
+
+        } else {
+
+            throw new IllegalArgumentException(
+                    "Invalid execution environment: "
+                            + executionEnv);
+        }
+
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+
+        driver.get(p.getProperty("appUrl"));
+
+        logger.info(
+                "Application launched: "
+                        + p.getProperty("appUrl"));
+    }
+
+    private WebDriver initializeLocalDriver(String browser) {
+
+        switch (browser.toLowerCase()) {
+
+            case "chrome":
+                logger.info("Launching Chrome browser");
+                return new ChromeDriver();
+
+            case "edge":
+                logger.info("Launching Edge browser");
+                return new EdgeDriver();
+
+            case "firefox":
+                logger.info("Launching Firefox browser");
+                return new FirefoxDriver();
+
+            default:
+                throw new IllegalArgumentException(
+                        "Unsupported browser: " + browser);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+	private WebDriver initializeRemoteDriver(
+            String browser,
+            String os)
+            throws MalformedURLException {
+
+        DesiredCapabilities capabilities =
+                new DesiredCapabilities();
+
+        /*
+         * Operating System
+         */
+        switch (os.toLowerCase()) {
+
+            case "windows":
+                capabilities.setPlatform(
+                        Platform.WINDOWS);
+                break;
+
+            case "linux":
+                capabilities.setPlatform(
+                        Platform.LINUX);
+                break;
+
+            case "mac":
+                capabilities.setPlatform(
+                        Platform.MAC);
+                break;
+
+            default:
+                throw new IllegalArgumentException(
+                        "Unsupported operating system: "
+                                + os);
+        }
+
+        /*
+         * Browser
+         */
+        switch (browser.toLowerCase()) {
+
+            case "chrome":
+                capabilities.setBrowserName("chrome");
+                break;
+
+            case "firefox":
+                capabilities.setBrowserName("firefox");
+                break;
+
+            case "edge":
+                capabilities.setBrowserName(
+                        "MicrosoftEdge");
+                break;
+
+            default:
+                throw new IllegalArgumentException(
+                        "Unsupported browser: "
+                                + browser);
+        }
+
+        String gridUrl =
+                p.getProperty(
+                        "gridUrl",
+                        "http://localhost:4444/wd/hub");
+
+        logger.info(
+                "Connecting to Selenium Grid: "
+                        + gridUrl);
+
+        return new RemoteWebDriver(
+                new URL(gridUrl),
+                capabilities);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+
+        if (driver != null) {
+
+            logger.info("Closing browser");
+
+            driver.quit();
+
+            driver = null;
+        }
+    }
+
+    /*
+     * Generates random alphabetic text.
+     */
+    public String randomString() {
+
+        return RandomStringUtils
+                .randomAlphabetic(5);
+    }
+
+    /*
+     * Generates random alphanumeric password.
+     */
+    public String randomAlphaNumeric() {
+
+        return RandomStringUtils
+                .randomAlphanumeric(8)
+                + "@1";
+    }
+
+    /*
+     * Generates random 10-digit number.
+     */
+    public String randomNumber() {
+
+        return RandomStringUtils
+                .randomNumeric(10);
+    }
+
+    /*
+     * Capture screenshot when a test fails.
+     */
+    public static String screenCapture(
+            String testName)
+            throws IOException {
+
+        DateTimeFormatter formatter =
+                DateTimeFormatter
+                        .ofPattern(
+                                "yyyyMMdd_HHmmss");
+
+        String timeStamp =
+                LocalDateTime
+                        .now()
+                        .format(formatter);
+
+        TakesScreenshot screenshot =
+                (TakesScreenshot) driver;
+
+        File source =
+                screenshot.getScreenshotAs(
+                        OutputType.FILE);
+
+        File screenshotDirectory =
+                new File("./screenshots");
+
+        if (!screenshotDirectory.exists()) {
+            screenshotDirectory.mkdirs();
+        }
+
+        File destination =
+                new File(
+                        screenshotDirectory,
+                        testName
+                                + "_"
+                                + timeStamp
+                                + ".png");
+
+        FileUtils.copyFile(
+                source,
+                destination);
+
+        return destination.getAbsolutePath();
+    }
 }
